020a51d2c824a7141e482c97eace52c8098b254c
Sreelakshmi T U <149130349+sreelakshmi2312@users.noreply.github.com>
2024-02-29 13:51:32 +0900

    Added:function to convert class object to map and back

--- lib/Models/listModel.dart
+++ lib/Models/listModel.dart
@@ -5,5 +5,22 @@ class StudentModel{
    int?id;
 
    StudentModel({required this.name,required this.age,this.id});
+
+
+   Map<String, dynamic> toMap(){
+    return{
+      'id':id,
+      'name':name,
+      'age':age,
+    };
+   }
+  
+  factory StudentModel.fromMap(Map<String, dynamic> map){
+    return StudentModel(
+      name: map['name'], 
+      age: map['age'] , 
+      id:map['id']);
+  }
+
 }
 
